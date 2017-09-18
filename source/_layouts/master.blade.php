652ebe26f1861848be1ff3cfef77291519331614
luisjg <luisjguzman87@gmail.com>
2017-09-18 14:08:47 -0700

    Add the wrapper class

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -9,13 +9,14 @@
     <meta name="description" content="Web Services that delivers public information about CSUN Campus">
     <script src="//use.typekit.net/gfb2mjm.js"></script>
     <script>try{Typekit.load();}catch(e){}</script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic">
     <link rel="stylesheet" href="//cdn.metalab.csun.edu/metaphor/css/metaphor.css">
   </head>
   <body>
     @include('_partials.site-header')
-    @yield('body')
+    <div class="main main--metalab">
+        @yield('body')
+    </div>
     @include('_partials.csun-footer')
     @include('_partials.metalab-footer')
     <script src="//cdn.metalab.csun.edu/metaphor/js/metaphor.js"></script>
